99da4002a4d20e769265b10edbcea5afee4e049b
KatyaJS <109916611+KatyaJS@users.noreply.github.com>
2023-03-23 13:37:51 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+# параметры для авторизации
+
+valid_email = "ami****@li**.ru"
+valid_phone = "+7978********"
+valid_pass = "Ti******"
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privac
